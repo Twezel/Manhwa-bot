15faf0e5695e029b4d84628c7c48545f755433c8
Twezel <bamyn187@gmail.com>
2026-05-01 22:14:54 +0100

    Import os and set port for app.run

--- bot.py
+++ bot.py
@@ -195,6 +195,8 @@ def webhook():
     return "ok"
 
 # ================= START =================
-int(os.environ.get("PORT", 10000))
+import os
+
+port = int(os.environ.get("PORT", 10000))
 
 app.run(host="0.0.0.0", port=port)
